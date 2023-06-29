0af21efad82e0383b94b27deedb04264a3767ab6
youssef <youssefboulaich33@gmail.com>
2023-06-29 15:27:26 +0200

    scss

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
+const containerDisplay = document.querySelector(".containerCards");
+
 async function getData() {
   try {
     const response = await axios.get(
       "https://character-database.becode.xyz/characters"
     );
     const data = response.data;
+
     data.forEach((element) => {
-      let displaySingleChar = document.createElement("div");
-      displaySingleChar.innerText = element.name;
-      document.querySelector(".containerCards").append(displaySingleChar);
+      // create container for each char
+      const displaySingleChar = document.createElement("div");
+      displaySingleChar.textContent = element.name;
+      containerDisplay.appendChild(displaySingleChar);
+
+      // create content for each container
+
+      // IMG and its content
+      const imgSingleChar = document.createElement("img");
+      imgSingleChar.className = "charImage";
+      imgSingleChar.src = `data:image/png;base64, ${element.image}`;
+      imgSingleChar.alt = "image of the character";
+
+      // SHORT DESCRIPTION OF CHARACTER AND ITS CONTENT
+      const shortDescriptionSingleChar = document.createElement("p");
+      shortDescriptionSingleChar.className = "short-description-character";
+      shortDescriptionSingleChar.textContent = element.shortDescription;
+
+      // NAME OF CHARACTER AND ITS CONTENT
+      const nameSingleChar = document.createElement("p");
+      nameSingleChar.className = "name-character";
+      nameSingleChar.textContent = element.name;
+
+      // APPEND EVERYTHING INTO THE DISPLAY CONTAINER
+      displaySingleChar.appendChild(imgSingleChar);
+      displaySingleChar.appendChild(nameSingleChar);
+      displaySingleChar.appendChild(shortDescriptionSingleChar);
     });
   } catch (error) {
     console.log(error);
   }
 }
+
 getData();
